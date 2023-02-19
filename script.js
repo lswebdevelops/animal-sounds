03e9a5ceb36a64ec84e5a99ece07f44bd937b048
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-02-19 10:43:04 -0300

    changing html

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-// define the array of animal names for reference
-const animalNames = ["cat.jpg", "chicken.jpg", 
-"cow.jpg", "dog.jpg", "duck.jpg",
-"goat.jpg", "goose.jpg", "horse.jpg", "pig.jpg", 
-"rooster.jpg", "sheep.jpg", "turkey.jpg"];
+const catImg = document.querySelector('#image1');
+const chickenImg = document.querySelector('#image2');
+const cowImg = document.querySelector('#image3');
+const dogImg = document.querySelector('#image4');
+const duckImg = document.querySelector('#image5');
+const goatImg = document.querySelector('#image6');
+const gooseImg = document.querySelector('#image7');
+const horseImg = document.querySelector('#image8');
+const pigImg = document.querySelector('#image9');
+const roosterImg = document.querySelector('#image10');
+const sheepImg = document.querySelector('#image11');
+const turkeyImg = document.querySelector('#image12');
 
-// 
